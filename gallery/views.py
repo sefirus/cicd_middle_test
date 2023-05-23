edf91b4b6ab0ccf32b75313b37385ef38db87a26
sefirus <sashasam348@gmail.com>
2023-05-23 12:36:11 +0300

    Implemented task

--- gallery/views.py
+++ gallery/views.py
@@ -1,1 +1,13 @@
 from django.shortcuts import render, get_object_or_404
+
+from gallery.models import Category, Image
+
+
+def gallery_view(request):
+    categories = Category.objects.all()
+    return render(request, 'gallery.html', {'categories': categories})
+
+
+def image_detail(request, pk: int):
+    image = Image.objects.filter(pk=pk).first()
+    return render(request, 'image_detail.html', {'image': image})
